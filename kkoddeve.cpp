c2514cf1026d4dc6a5e713b32d21b08f5716d2ba
Kavya Kumar <116029008+kavyak1604@users.noreply.github.com>
2023-10-08 12:11:16 +0530

    Add files via upload

---
+++ kkoddeve.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main()
+{  int num;
+   cout<<"enter an integer:"<<"\n";
+   cin >>num;
+   if(num&1)
+  {
+    cout<<"odd";}
+   else{
+    cout<<"even";
+ }
+
+
+}
