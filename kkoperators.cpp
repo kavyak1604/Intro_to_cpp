feb5120545f0a3e91e5a4ba3fdca1b778d374e29
Kavya Kumar <116029008+kavyak1604@users.noreply.github.com>
2023-10-08 12:06:07 +0530

    Add files via upload

---
+++ kkoperators.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int a; int b;int sum;
+int main()
+{
+    
+    
+    cout<<"Enter first number:";
+    cin >>a;
+    cout<<"Enter second number:";
+    cin >>b;
+    sum= a+b;
+    cout<<"The sum is:"<<sum<<endl;
+
+    
+    
+    cout<<"Enter first number:";
+    cin >>a;
+    cout<<"Enter second number:";
+    cin >>b;
+    int diff= a-b;
+    cout<<"The difference is:"<<diff<<endl;
+
+
+    cout<<"Enter first number:";
+    cin >>a;
+    cout<<"Enter second number:";
+    cin >>b;
+    int product= a*b;
+    cout<<"The product is:"<<product<<endl;
+
+    
+    cout<<"Enter first number:";
+    cin >>a;
+    cout<<"Enter second number:";
+    cin >>b;
+    int quotient= a/b;
+    cout<<"The quotient is:"<<quotient<<endl;
+
+    
+    cout<<"Enter first number:";
+    cin >>a;
+    cout<<"Enter second number:";
+    cin >>b;
+    int remainder= a%b;
+    cout<<"The remainder is:"<<remainder<<endl;
+
+}
